af7b27888c6446f79b9d40aacc723df3c1f64c9a
Daniel Coy <danielfelipe.coy927@comunidadunir.net>
2023-12-25 19:11:18 +0100

    commit3

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,13 +10,6 @@ const Header = ({}: LayoutProps) => {
 				<a href="/"  className="text-white text-2xl font-semibold">
 					<span className="text-yellow-300">Hot Chili</span> Library
 				</a>
-				
-				
-				<nav className="space-x-4">
-					<a href="/" className="text-white hover:text-yellow-300 transition duration-300">Home</a>
-					<a href="/books" className="text-white hover:text-yellow-300 transition duration-300">Books</a>
-					<a href="/contact" className="text-white hover:text-yellow-300 transition duration-300">Contact</a>
-				</nav>
 			</div>
 		</header>
 	);
